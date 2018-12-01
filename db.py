16506a45c9b1d052613c38c989225f314acec4b8
root <root@LAPTOP-JCD3LNVF.localdomain>
2018-12-01 16:31:20 -0500

    Added items

--- db.py
+++ db.py
@@ -12,7 +12,7 @@ class Budget(db.Model):
 
     def __init__(self, **kwargs):
         self.total = kwargs.get('total','')
-        self.spent = 0
+        self.spent = kwargs.get('spent','')
         self.username = kwargs.get('username', '')
 
     def serialize(self):
@@ -28,11 +28,33 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     total = db.Column(db.Integer)
+    items = db.relationship('Item', cascade='delete')
+    budget_id = db.Column(db.Integer, db.ForeignKey('budget.id'), nullable=False)
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name', '')
+        self.total = kwargs.get('total','')
+        self.budget_id = kwargs.get('budget_id')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'total': self.total
+        }
+
+class Item(db.Model):
+    __tablename__ = 'item'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    total = db.Column(db.Integer)
+    category_name = db.Column(db.Integer, db.ForeignKey('category.name'), nullable=False) 
     budget_id = db.Column(db.Integer, db.ForeignKey('budget.id'), nullable=False)
 
     def __init__(self, **kwargs):
         self.name = kwargs.get('name', '')
         self.total = kwargs.get('total', '')
+        self.category_name = kwargs.get('category_name')
         self.budget_id = kwargs.get('budget_id')
 
     def serialize(self):
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 import json
-from db import db, Budget, Category
+from db import db, Budget, Category, Item
 from flask import Flask, request
 
 db_filename = "todo.db"
@@ -20,6 +20,12 @@ def get_budgets():
     res = {'success': True, 'data': [budget.serialize() for budget in budgets]} 
     return json.dumps(res), 200
 
+@app.route('/api/budget/', methods=['DELETE'])
+def delete_all():
+    Category.drop_all()
+    db.session.commit()
+    return json.dumps({'success':True})
+
 @app.route('/api/budget/', methods=['POST'])
 def create_budget():
     budget_body = json.loads(request.data)
@@ -27,9 +33,9 @@ def create_budget():
     if budget is None: 
         budget = Budget(
 	   total = budget_body.get('total'),
+           spent = 0,
            username = budget_body.get('username')
         )
-	
         db.session.add(budget)
     else:
        budget.total = budget_body.get('total') 
@@ -41,8 +47,8 @@ def get_budget(budget_id):
     budget = Budget.query.filter_by(id=budget_id).first() 
     if budget is not None:
         return json.dumps({'success': True, 'data': budget.serialize()}), 200
-    return json.dumps({'success': False, 'error': 'Budget not found!'}), 404  
-	
+    return json.dumps({'success': False, 'error': 'Budget not found!'}), 404
+
 @app.route('/api/budget/<int:budget_id>/<string:category_name>/', methods=['POST'])
 def spend_budget(budget_id, category_name):
     budget = Budget.query.filter_by(id=budget_id).first()
@@ -50,15 +56,33 @@ def spend_budget(budget_id, category_name):
         category = Category.query.filter_by(budget_id=budget_id, name=category_name).first()
         if category is not None:
             budget_body = json.loads(request.data)
-            budget.spent += budget_body.get('spent', budget.spent)
-            category.total += budget_body.get('spent', budget.spent)
+            spent = budget_body.get('spent', budget.spent)
+            item = Item(
+                name = budget_body.get('name'),
+                total = spent,
+                category_id = category.id,
+                budget_id = budget.id
+            )
+            budget.spent += spent
+            category.total += spent
             if budget.spent <= budget.total:
+                category.items.append(item)
+                db.session.add(item)
                 db.session.commit()
-            else:
-                return json.dumps({'success': False, 'error': 'You are spending overbudget!'})
-        else:
-            return json.dumps({'success': False, 'error': 'Category not found!'}), 404
-        return json.dumps({'success': True, 'data': budget.serialize()}), 200
+                return json.dumps({'success': True, 'data': item.serialize()}), 200
+            return json.dumps({'success': False, 'error': 'You are spending overbudget!'})
+        return json.dumps({'success': False, 'error': 'Category not found!'}), 404
+    return json.dumps({'success': False, 'error': 'Budget not found!'}), 404
+
+@app.route('/api/budget/<int:budget_id>/<string:category_name>/')
+def get_items(budget_id, category_name):
+    budget = Budget.query.filter_by(id=budget_id).first()
+    if budget is not None:
+        category = Category.query.filter_by(id=budget_id, name=category_name).first()
+        if category is not None:
+            items = [item.serialize() for item in category.items]
+            return json.dumps({'success': True, 'data': items}), 200
+        return json.dumps({'success': False, 'error': 'Category not found!'}), 404
     return json.dumps({'success': False, 'error': 'Budget not found!'}), 404
 
 @app.route('/api/budget/<int:budget_id>/', methods=['DELETE'])
@@ -76,23 +100,57 @@ def get_categories(budget_id):
     if budget is not None:
         categories = [category.serialize() for category in budget.categories]
         return json.dumps({'success': True, 'data': categories}), 200
-    return json.dumps({'success': False, 'error': 'Budget not found!'}), 404 
-	
+    return json.dumps({'success': False, 'error': 'Budget not found!'}), 404
+
 @app.route('/api/budget/<int:budget_id>/category/', methods=['POST'])
 def create_category(budget_id):
     budget = Budget.query.filter_by(id=budget_id).first()
     if budget is not None:
         category_body = json.loads(request.data)
-        category = Category(
-            name = category_body.get('name'),
-            total = 0,
-            budget_id = budget.id
-        )
-        budget.categories.append(category)
-        db.session.add(category)
-        db.session.commit()
-        return json.dumps({'success': True, 'data': category.serialize()}), 201
-    return json.dumps({'success': False, 'error': 'Budget not found!'}), 404 
+        category_name = category_body.get('name')
+        category = Category.query.filter_by(id=budget_id, name=category_name).first()
+        if category is None:
+            category = Category(
+                name = category_name,
+                total = 0,
+                budget_id = budget.id
+            )
+            budget.categories.append(category)
+            db.session.add(category)
+            db.session.commit()
+            return json.dumps({'success': True, 'data': category.serialize()}), 201
+        return json.dumps({'success': False, 'error': 'Category already exists!'}), 201
+    return json.dumps({'success': False, 'error': 'Budget not found!'}), 404
+
+@app.route('/api/budget/<int:budget_id>/<string:category_name>/<int:item_id>/', methods=['DELETE'])
+def delete_it(budget_id, category_name, item_id):
+    budget = Budget.query.filter_by(id=budget_id).first()
+    if budget is not None:
+        category = Category.query.filter_by(budget_id=budget_id, name=category_name).first()
+        if category is not None:
+            item = Item.query.filter_by(id=item_id, budget_id=budget_id, category_name=category_name).first()
+            if item is not None:
+                budget.spent -= item.total
+                category.total -= item.total
+                db.session.delete(item)
+                db.session.commit()
+                return json.dumps({'success': True, 'data': budget.serialize()}), 200
+            return json.dumps({'success': False, 'error': 'Item not found!'}), 404
+        return json.dumps({'success': False, 'error': 'Category not found!'}), 404
+    return json.dumps({'success': False, 'error': 'Budget not found!'}), 404
+
+@app.route('/api/budget/<int:budget_id>/<string:category_name>/', methods=['DELETE'])
+def delete_category(budget_id, category_name):
+    budget = Budget.query.filter_by(id=budget_id).first()
+    if budget is not None:
+        category = Category.query.filter_by(budget_id=budget_id, name=category_name).first()
+        if category is not None:
+            budget.spent -= category.total
+            db.session.delete(category)
+            db.session.commit()
+            return json.dumps({'success': True, 'data': budget.serialize()}), 200
+        return json.dumps({'success': False, 'error': 'Category not found!'}), 404
+    return json.dumps({'success': False, 'error': 'Budget not found!'}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
